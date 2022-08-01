94560c6eacaba3ad55d932d043b11c3dbf406d4a
unknown <shahidiam83@gmail.com>
2022-08-02 00:32:49 +0430

    append actions code

--- resources/js/context/actions.js
+++ resources/js/context/actions.js
@@ -1,14 +1,37 @@
 
-export function login(dispatch){
+export async function login(dispatch,credentials){
+	
+	dispatch({type:'REQUEST_LOGIN'});
+	let response = await axios.post('/api/login',credentials);
+	response.catch(function(error){
+		dispatch({type:'LOGIN_ERROR',error:error});
+	});
 
+	if(response.data.user){
+		dispatch({type:'LOGIN_SUCCESS'});
+		localStorage.setItem('currentUser',JSON.stringify(data));
+		return data;
+	}
+
+	
 }
 
-export function register(dispatch){
+export async function register(dispatch,credentials){
 
+	dispatch({type:'REQUEST_REGISTER'});
+	let response = await.post('/api/register',credentials);
+	response.catch(function(error){
+		dispatch({type:'REGISTER_ERROR',error:error});
+	});
+
+	if(response.data.user){
+		dispatch({type:'REGISTER_SUCCESS'});
+	}
 }
 
 export function logout(dispatch){
-
+	dispatch({type:'LOGOUT'});
+	localStorage.removeItem('currentUser');
 }
 
 
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
+Route::post('/logout',[AuthController::class,'logout']);
+
 
 Route::post('/post/new',[PostController::class,'newPost']);
 Route::post('/post/edit',[PostController::class,'getPost']);
